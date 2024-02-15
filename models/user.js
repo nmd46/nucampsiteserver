a86e9b8caf40fd0959972332589aee281d3e0889
Nicole Durham <nicole.martinez.durham@gmail.com>
2024-02-15 16:16:46 -0500

    Passport

--- models/user.js
+++ models/user.js
@@ -1,20 +1,14 @@
 const mongoose = require("mongoose");
+const passportLocaLMongoose = require("passport-local-mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
   admin: {
     type: Boolean,
     default: false,
   },
 });
+
+userSchema.plugin(passportLocaLMongoose);
 
 module.exports = mongoose.model("User", userSchema);
